69337e84a393b2a15b54744d9081436465c33a00
Laurens <laurens.schilders@gmail.com>
2020-03-21 23:16:38 +0100

    Add corona app

---
+++ static/corona/precache-manifest.96eb76e3c413592f203037ca27d3e7b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9d1ccdcef3cbf71984a58b81316e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b73a5ff7fb70488383",
+    "url": "/static/css/main.2342a912.chunk.css"
+  },
+  {
+    "revision": "0fb7e97c4a32bd74b1b0",
+    "url": "/static/js/2.ff4b6c89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff4b6c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b73a5ff7fb70488383",
+    "url": "/static/js/main.29be8a64.chunk.js"
+  },
+  {
+    "revision": "09eb2fae383ea5eae762",
+    "url": "/static/js/runtime-main.074d3413.js"
+  },
+  {
+    "revision": "7bef4dde56e9fb2bff86950171c8341c",
+    "url": "/static/media/belgie.7bef4dde.png"
+  }
+]);
